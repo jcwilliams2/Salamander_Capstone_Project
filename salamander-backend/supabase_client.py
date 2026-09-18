8afb761ebf82c4333cbd814660bb25fbc808d200
Jerlisa Williams <124515633+jcwilliams2@users.noreply.github.com>
2026-09-18 16:30:01 -0400

    create supabase file for client creation

---
+++ salamander-backend/supabase_client.py
@@ -0,0 +1,10 @@
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase = create_client(
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+)
